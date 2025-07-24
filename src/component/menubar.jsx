0d0d47ba9007c754ce546c3f2667da8e6b9ffed4
Jovinroshandev <jovinroshandev@gmail.com>
2025-07-24 15:51:55 +0530

    first commi

---
+++ src/component/menubar.jsx
@@ -0,0 +1,9 @@
+export default function Menubar(){
+    return(
+        <>
+        <div className="bg-blue-900">
+            <h1 className="text-lg text-white font-bold">SkyGod</h1>
+        </div>
+        </>
+    )
+}
